4b295785c29d7cbc219855ec086267be5e50a677
Joao Vitor B Lourenco de Almeida <jvitor.almeida98@apoioprodesp.sp.gov.br>
2020-09-08 10:55:04 -0300

    first commit

---
+++ api/src/test/java/br/com/lacuna/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lacuna.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
